6fdc48c7a77a5d6fde32509f46c8aabf5d39e1e4
Dharmin <dharmingeo@gmail.com>
2019-01-29 21:20:23 +0100

    Updated baking stage initialiser from right-brothers

--- src/main/java/org/maas/PackagingStageInitializer.java
+++ src/main/java/org/maas/PackagingStageInitializer.java
@@ -1,19 +1,53 @@
 package org.maas;
 
 import java.util.Vector;
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+import com.fasterxml.jackson.core.type.TypeReference;
+
 import org.maas.Initializer;
+import org.maas.utils.JsonConverter;
+import org.maas.data.models.Bakery;
 
 public class PackagingStageInitializer extends Initializer {
     @Override
     public String initialize(String scenarioDirectory) {
+        Vector<String> bakeryNames = this.getBakeryNames(scenarioDirectory);
         Vector<String> agents = new Vector<>();
 
-        agents.add("dummy:org.right_brothers.agents.PackagingStageTester");
-        agents.add("postBakingProcessor:org.right_brothers.agents.PreLoadingProcessor");
-        agents.add("packaging-agent:org.right_brothers.agents.LoadingBayAgent");
+        for (String bakeryName : bakeryNames) {
+            agents.add(bakeryName + "-preLoadingProcessor:org.right_brothers.agents.PreLoadingProcessor(" + bakeryName + ", " + scenarioDirectory + ", single-stage)");
+            agents.add(bakeryName + "-packaging-agent:org.right_brothers.agents.ProductBoxerAgent(" + bakeryName + ", " + scenarioDirectory + ")");
+            agents.add(bakeryName + "-loader-agent:org.maas.agents.LoadingBayAgent(" + bakeryName + ")");
+        }
 
         String agentInitString = String.join(";", agents);
         agentInitString += ";";
         return agentInitString;
+    }
+    private Vector<String> getBakeryNames (String scenarioDirectory) {
+        String filePath = "config/" + scenarioDirectory + "/bakeries.json";
+        String fileString = this.readConfigFile(filePath);
+        TypeReference<?> type = new TypeReference<Vector<Bakery>>(){};
+        Vector<Bakery> bakeries = JsonConverter.getInstance(fileString, type);
+        Vector<String> bakeryNames = new Vector<String> (bakeries.size());
+        for (Bakery bakery : bakeries) {
+            bakeryNames.add(bakery.getGuid());
+        }
+        return bakeryNames;
+    }
+    private String readConfigFile (String filePath){
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource(filePath).getFile());
+        String fileString = "";
+        try (Scanner sc = new Scanner(file)) {
+            sc.useDelimiter("\\Z"); 
+            fileString = sc.next();
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileString;
     }
 }
